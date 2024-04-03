d2d7f590291bc4d0f9b477245ed8435ed974d83e
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2024-04-03 14:45:16 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { getDataFromAPI } from './js/pixabay-api';
+import { renderGalleryImg } from './js/render-functions';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
