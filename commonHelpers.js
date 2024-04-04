664f166a05501369fc2598ab928d1ddff2bf1af5
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2024-04-04 12:13:10 +0000

    Deploying to gh-pages from @ YanaKorniichuk25/goit-js-hw-11@89e4387fc294ef15e818cba2ba00c97f2da2f0b1 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,9 @@
-import{S as f,i as g}from"./assets/vendor-7659544d.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(e){if(e.ep)return;e.ep=!0;const i=s(e);fetch(e.href,i)}})();function d(t,r,s){const a=p(t,r,s);return fetch(a).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).catch(e=>(console.log("ERROR:",e),Promise.reject(e)))}function p(t,r,s){const a=new URLSearchParams({key:r,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});return`${t}?${a}`}const m=new f(".gallery a",{captionsData:"alt",captionDelay:250});function h(t,r){t.innerHTML=y(r),m.refresh()}function y(t){return t.map(({webformatURL:r,largeImageURL:s,tags:a,likes:e,views:i,comments:o,downloads:u})=>` <li class="gallery-item">
-      <a class="gallery-link" href="${s}">
+import{S as f,i as g}from"./assets/vendor-7659544d.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function a(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(e){if(e.ep)return;e.ep=!0;const i=a(e);fetch(e.href,i)}})();function d(t,r,a){const o=p(t,r,a);return fetch(o).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).catch(e=>(console.log("ERROR:",e),Promise.reject(e)))}function p(t,r,a){const o=new URLSearchParams({key:r,q:a,image_type:"photo",orientation:"horizontal",safesearch:"true"});return`${t}?${o}`}const m=new f(".main-gallery a",{captionsData:"alt",captionDelay:250});function h(t,r){t.innerHTML=y(r),m.refresh()}function y(t){return t.map(({webformatURL:r,largeImageURL:a,tags:o,likes:e,views:i,comments:s,downloads:u})=>` <li class="gallery-item-container">
+      <a class="gallery-link" href="${a}">
         <figure class="gallery-figure">
-          <img class="gallery-image" src="${r}" alt="${a}" width="360" />
+          <img class="gallery-image" src="${r}" alt="${o}" width="360" />
           <figcaption class="gallery-figcaption">
-            <ul class="gallery-text">
+            <ul class="gallery-info">
               <li>
                 <span>Likes</span>
                 <p>${e}</p>
@@ -14,7 +14,7 @@ import{S as f,i as g}from"./assets/vendor-7659544d.js";(function(){const r=docum
               </li>
               <li>
                 <span>Comments</span>
-                <p>${o}</p>
+                <p>${s}</p>
               </li>
               <li>
                 <span>Downloads</span>
@@ -24,5 +24,5 @@ import{S as f,i as g}from"./assets/vendor-7659544d.js";(function(){const r=docum
           </figcaption>
         </figure>
       </a>
-    </li>`).join(" ")}const L="/goit-js-hw-11/assets/icon-error-4f06a8ee.svg",E="43227230-2cc9b082dfeccb819f6787c2c",b="https://pixabay.com/api/",F=document.querySelector(".search-form"),c=document.querySelector(".gallery"),n=document.querySelector(".loader-wrapper ");F.addEventListener("submit",S);function S(t){t.preventDefault(),c.innerHTML="",n.classList.remove("is-hidden");const r=t.currentTarget.search.value.trim();if(!r){l("Please enter a value in the field!","Error"),t.currentTarget.reset(),n.classList.add("is-hidden");return}d(b,E,r).then(s=>{const a=s.hits;if(a.length===0){l("Sorry, there are no images matching your search query. Please try again!"),n.classList.add("is-hidden");return}h(c,a),n.classList.add("is-hidden")}).catch(s=>{l("Error fetching data. Please try again later","Error"),console.error("Error fetching data:",s)}),t.currentTarget.reset()}const P={position:"topRight",titleColor:"#FFF",titleSize:"16",titleLineHeight:"24",messageColor:"#FFF",messageSize:"16",messageLineHeight:"24"};function l(t,r){g.error({...P,title:r||"",message:`${t}`,backgroundColor:"#EF4040",iconUrl:L})}
+    </li>`).join(" ")}const L="/goit-js-hw-11/assets/icon-error-4f06a8ee.svg",b="43227230-2cc9b082dfeccb819f6787c2c",E="https://pixabay.com/api/",F=document.querySelector(".search-box"),c=document.querySelector(".main-gallery"),n=document.querySelector(".loader-wrapper ");F.addEventListener("submit",S);function S(t){t.preventDefault(),c.innerHTML="",n.classList.remove("is-hidden");const r=t.currentTarget.search.value.trim();if(!r){l("Please enter a value in the field!","Error"),t.currentTarget.reset(),n.classList.add("is-hidden");return}d(E,b,r).then(a=>{const o=a.hits;if(o.length===0){l("Sorry, there are no images matching your search query. Please try again!"),n.classList.add("is-hidden");return}h(c,o),n.classList.add("is-hidden")}).catch(a=>{l("Error fetching data. Please try again later","Error"),console.error("Error fetching data:",a)}),t.currentTarget.reset()}const P={position:"topRight",titleColor:"#FFF",titleSize:"16",titleLineHeight:"24",messageColor:"#FFF",messageSize:"16",messageLineHeight:"24"};function l(t,r){g.error({...P,title:r||"",message:`${t}`,backgroundColor:"#EF4040",iconUrl:L})}
 //# sourceMappingURL=commonHelpers.js.map
